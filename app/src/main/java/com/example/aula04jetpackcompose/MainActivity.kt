133f6bc83d5a7aa803fdb53842f3f3dad3f8e090
Alex Kutzke <alexkutzke@gmail.com>
2025-04-01 15:10:42 -0300

    Ready for exercise 1

--- app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -20,10 +21,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             Aula04JetPackComposeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface(modifier= Modifier.padding(innerPadding)){
+                        SimpleScreen()
+                    }
                 }
             }
         }
@@ -31,28 +31,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun SimpleScreen() {
+    Text("Aula 04!")
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun SimpleScreenPreview() {
     Aula04JetPackComposeTheme {
-        Greeting("Android")
+        SimpleScreen()
     }
-}
-
-@Composable
-fun MessageCard(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview
-@Composable
-fun PreviewMessageCard() {
-    MessageCard("Android")
 }
