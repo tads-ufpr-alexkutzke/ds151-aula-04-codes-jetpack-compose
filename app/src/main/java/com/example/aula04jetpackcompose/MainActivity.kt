77d707c091bc8ed4e8142aaf9fdd045cc44aea99
Alex Kutzke <alexkutzke@gmail.com>
2025-04-01 14:34:08 -0300

    MessgeCard

--- app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
@@ -45,3 +45,14 @@ fun GreetingPreview() {
         Greeting("Android")
     }
 }
+
+@Composable
+fun MessageCard(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview
+@Composable
+fun PreviewMessageCard() {
+    MessageCard("Android")
+}
