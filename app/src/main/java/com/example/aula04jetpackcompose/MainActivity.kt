8249adb8124fcda5e233e8820649cd3ae94edcb3
Alex Kutzke <alexkutzke@gmail.com>
2025-04-01 13:57:27 -0300

    SimpleList

--- app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
@@ -6,11 +6,14 @@ import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aula04jetpackcompose.ui.theme.Aula04JetPackComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -43,5 +46,25 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
 fun GreetingPreview() {
     Aula04JetPackComposeTheme {
         Greeting("Android")
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun SimpleListPreview() {
+    Aula04JetPackComposeTheme {
+        SimpleList()
+    }
+}
+
+// Adicionar o seguinte import manualmente caso necessario
+// import androidx.compose.foundation.lazy.items
+@Composable
+fun SimpleList() {
+    val items = listOf("Item 1", "Item 2", "Item 3")
+    LazyColumn {
+        items(items) { item ->
+            Text(text = item, modifier = Modifier.padding(8.dp))
+        }
     }
 }
