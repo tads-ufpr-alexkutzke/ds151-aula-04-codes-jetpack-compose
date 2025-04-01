89a879d783692a82daac53c7df9f3a61452fd747
Alex Kutzke <alexkutzke@gmail.com>
2025-04-01 13:40:42 -0300

    README

---
+++ README.md
@@ -0,0 +1,5 @@
+# DS151 - Aula 04 - Jetpack Compose
+
+Código da aula 04 de DS151 - Desenvolvimento Mobile.
+
+Exemplos do Jetpack Compose.
---
+++ app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.example.aula04jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.aula04jetpackcompose.ui.theme.Aula04JetPackComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Aula04JetPackComposeTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Aula04JetPackComposeTheme {
+        Greeting("Android")
+    }
+}
