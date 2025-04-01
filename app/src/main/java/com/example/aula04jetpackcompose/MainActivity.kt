81f7d77097823056e066ee0ca304d85746fbaa3a
Alex Kutzke <alexkutzke@gmail.com>
2025-04-01 14:04:07 -0300

    LoginForm

--- app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/example/aula04jetpackcompose/MainActivity.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aula04jetpackcompose.ui.theme.Aula04JetPackComposeTheme
@@ -68,3 +77,39 @@ fun SimpleList() {
         }
     }
 }
+
+@Preview(showBackground = true)
+@Composable
+fun LoginFormPreview() {
+    Aula04JetPackComposeTheme {
+        LoginForm()
+    }
+}
+
+
+@Composable
+fun LoginForm() {
+    var username by remember { mutableStateOf("") }
+    var password by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier.padding(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        TextField(
+            value = username,
+            onValueChange = { username = it },
+            label = { Text("Usuário") }
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            label = { Text("Senha") },
+            visualTransformation = PasswordVisualTransformation()
+        )
+        Button(onClick = { /* Lógica de login aqui */ }) {
+            Text("Entrar")
+        }
+    }
+}
+
